274dd9fabd1741ac1f208e26a51b058709fe19d0
Unknown <sebastianhruby@gmail.com>
2017-12-16 19:15:27 +0100

    creating layout for forms

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,4 +1,9 @@
 class BuildingsController < ApplicationController
+
+  def show
+    @building = Building.find(params[:id])
+  end
+
   def index
     @buildings = Building.all
   end
@@ -8,6 +13,14 @@ class BuildingsController < ApplicationController
   end
 
   def create
+    @building = Building.create(building_params)
+    redirect_to @building
+  end
 
+
+  private
+
+  def building_params
+    params.require(:building).permit(:title, :code)
   end
 end

1179f33058f9606d4552165d4e4e2489ad68999f
Unknown <sebastianhruby@gmail.com>
2017-12-16 21:32:41 +0100

    adding courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,12 @@
 Rails.application.routes.draw do
-  get 'rooms/index'
-
-  get 'rooms/show'
-
-  get 'rooms/new'
-
-  get 'rooms/create'
-
-  get 'rooms/edit'
-
-  get 'rooms/update'
-
-  get 'rooms/destroy'
-
-  get 'buildings/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'buildings#index'
 
   resources :buildings do
-    resources :rooms
+    resources :rooms do
+      resources :courses
+    end
   end
 
 

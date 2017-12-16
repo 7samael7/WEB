9b0e1f11cfbf29edb65bbdd000e3b87541c4d8d0
Unknown <sebastianhruby@gmail.com>
2017-12-16 16:01:06 +0100

    trying erd

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,6 @@ class Course < ApplicationRecord
   has_many :teacher_assignments
   has_many :student_assignments
 
-  has_one enum Language: [:CZECH, :ENGLISH]
-  has_one enum StudyType: [:FULL_TIME, :PART_TIME]
+  enum Language: [:CZECH, :ENGLISH]
+  enum StudyType: [:FULL_TIME, :PART_TIME]
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
   has_many :student_assignments
 
-  has_one enum StudyType: [:FULL_TIME, :PART_TIME]
+  enum StudyType: [:FULL_TIME, :PART_TIME]
 end

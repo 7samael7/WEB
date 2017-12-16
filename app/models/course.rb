60f7f14b2ca3bd421fd63811c24b153c893813c7
Unknown <sebastianhruby@gmail.com>
2017-12-16 17:20:31 +0100

    renaming enums

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,6 @@ class Course < ApplicationRecord
   has_many :teacher_assignments
   has_many :student_assignments
 
-  enum Language: [:CZECH, :ENGLISH]
-  enum StudyType: [:FULL_TIME, :PART_TIME]
+  enum language: [:CZECH, :ENGLISH]
+  enum study_type: [:FULL_TIME, :PART_TIME]
 end
